cb0bed903247bdbc69a2b0b6963114433cdbeff5
Bárbara Cassiram <cassiram15@gmail.com>
2025-05-04 02:03:06 -0300

    add: se agrega interfaz repository

---
+++ challenge/src/main/java/com/itba/challenge/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.itba.challenge.repository;
+
+import com.itba.challenge.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
