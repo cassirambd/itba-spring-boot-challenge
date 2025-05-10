e27506d372c9ae10d33e1c5d3eef2589dbe756f4
Bárbara Cassiram <cassiram15@gmail.com>
2025-05-10 16:02:11 -0300

    fix: corregir error de application.properties y agregar .env

--- challenge/src/main/java/com/itba/challenge/ChallengeApplication.java
+++ challenge/src/main/java/com/itba/challenge/ChallengeApplication.java
@@ -1,5 +1,6 @@
 package com.itba.challenge;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,12 @@ public class ChallengeApplication {
 
     public static void main(String[] args) {
         log.info("Starting challenge application...");
+
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
         SpringApplication.run(ChallengeApplication.class, args);
         log.info("Challenge application started!");
     }
--- challenge/src/test/java/com/itba/challenge/ChallengeApplicationTests.java
+++ challenge/src/test/java/com/itba/challenge/ChallengeApplicationTests.java
@@ -1,13 +1,24 @@
 package com.itba.challenge;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChallengeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    static void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().directory("../")
+                .filename(".env")
+                .load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
 
+    @Test
+    void contextLoads() {
+    }
 }
